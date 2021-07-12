7df0df8b7ec24c04994be1318f992e12a34a28a8
Giovanna Niemeyer <giovanna.niemeyer@gmail.com>
2021-07-12 15:29:22 -0300

    rotas implementadas no back

--- src/app.js
+++ src/app.js
@@ -7,4 +7,25 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get('/items', async (req, res) => {
+    const resp = await connection.query(`
+        SELECT * FROM shops  
+    `)
+    return res.status(200).send(resp.rows);
+});
+
+app.post('/items', async (req, res) => {
+
+    const {text} = req.body;
+
+    if(text?.length === 0 || typeof(text) !== String) return res.sendStatus(404);
+
+        await connection.query(`
+        INSERT INTO shops (text) VALUES ($1)`, [text])
+
+        return res.sendStatus(201);
+
+
+})
+
 export default app;
